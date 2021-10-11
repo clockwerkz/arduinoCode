21c2e5c746fa04fbff31e3f211503e2b304fb90f
Carlos <clockwerkz@gmail.com>
2021-10-11 11:39:21 -0700

    editing the color randomness to get more of a flame effect

--- jsck_o_latern_lighting/jsck_o_latern_lighting.ino
+++ jsck_o_latern_lighting/jsck_o_latern_lighting.ino
@@ -2,7 +2,7 @@
 #include "FastLED.h"
 
 const int PUMPKIN_COUNT = 4;
-const int LIGHT_COUNT = 4;
+const int LIGHT_COUNT = 7;
 const int MAX_LIGHTS = LIGHT_COUNT * PUMPKIN_COUNT;
 
 struct TimerObj {
@@ -16,7 +16,7 @@ TimerObj timers[PUMPKIN_COUNT];
 
 void setup() {
   // put your setup code here, to run once:
-  FastLED.addLeds<WS2812, 2, GRB>(leds, MAX_LIGHTS);
+  FastLED.addLeds<WS2811, 2, RGB>(leds, MAX_LIGHTS);
   for (int i=0; i < PUMPKIN_COUNT; i++){
     timers[i].timer = random(100) + 20;
   }
@@ -32,33 +32,32 @@ void loop() {
       int start = i * LIGHT_COUNT;
       int end = start + LIGHT_COUNT;
        updateLights(leds, start, end);
-       timers[i].timer = random(100) + 20;
+       timers[i].timer = random(100) + 60;
        timers[i].chrono.restart();
     }
   }
   FastLED.show();
 }
-
+//remove the colorChoiceBlue logic
 void updateLights(CRGB leds[], int start, int end){
-  for (int i=start; i < end; i++) {
-    if ((i / LIGHT_COUNT) % 2 == 0) {
-      leds[i]=ColorChoice();  
-    } else {
-      leds[i]=ColorChoiceBlue();
-    }
-     
+  int mid = end - 3;
+  for (int i=start; i < mid; i++) {
+      leds[i]=ColorChoice();      
+  }
+  for (int i=mid; i < end; i++){
+    leds[i] = CRGB(255,0,0);
   }
 }
-
+//red green blue - blue green red
 CRGB ColorChoice() {
-  int choice = random(3);
+  int choice = random(4);
     switch(choice){
       case 0:
-        return CRGB(255,0,0);
+        return CRGB(255,100,0);
       case 1:
-        return CRGB(51,10,0);
+        return CRGB(255,10,0);
       case 2:
-        return CRGB(51,17,0);
+        return CRGB(151,17,0);
     } 
   return CRGB(51,17,0); 
 }
